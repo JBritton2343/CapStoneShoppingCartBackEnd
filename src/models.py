503a0f2839bfa760cf9979f837aab68830727122
JBritton2343 <jbritton854@gmail.com>
2022-11-18 15:06:21 -0600

    added cart table to database

--- src/models.py
+++ src/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    cart=db.realtionship("cart")
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -25,6 +26,8 @@ class Items(db.Model):
     product_name = db.Column(db.String(150), nullable=False)
     quantity = db.Column(db.Integer, nullable=True)
     unit_price = db.Column(db.Float, nullable=False)
+    cart = db.relationship("Cart")
+
 
     def __repr__(self):
         return '<Items %r>' % self.product_name
@@ -35,18 +38,7 @@ class Items(db.Model):
             "item": self.product_name,
             "price": self.unit_price
         }
-
-class AltVendors(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    vendor_name = db.Column(db.String(250), nullable=False)
-    price = db.Column(db.Float, nullable=True)
-
-    def __repr__(self):
-        return '<AltVendor %r>' % self.vendor_name
-
-    def serialize(self):
-        return{
-            "id": self.id,
-            "vendor": self.vendor_name,
-            "price": self.price
-        }
+class Cart(db.model):
+    id = db.Column(db.Integer, primary_key="True")
+    user_id=db.Column(db.Integer, db.ForeignKey("user.id"))
+    products_id = db.Column(db.Integer, db.ForeignKey("product.id"))
